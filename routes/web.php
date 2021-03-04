8dcdb23564c68cf20e70b5d7978d5d21114dbf03
kube-csc <kube@kube-csc.de>
2021-03-04 11:24:13 +0100

    Erstellen der Startseite mit CSS und JS

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');

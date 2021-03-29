1a250a964eae495ec25aa465a8166f5bc1ba35c9
kube-csc <kube@kube-csc.de>
2021-03-30 01:06:03 +0200

    Seeder aus Git Ausgetragen

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        $this->call(InstructionSeeder::class);
-        $this->call(SportSectionSeeder::class);
+        
     }
 }

6e2bae7d3dcaa47ab724f7d6cb1c32a9ef5e4c97
kube-csc <kube@kube-csc.de>
2021-08-10 18:41:45 +0200

    Datenbank User angepasst

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,11 +18,11 @@ class CreateUsersTable extends Migration
           //Vereinsverwaltung
           $table->string('nachname', 40);
           $table->string('vorname', 40);
-          $table->string('geschlecht', 1);
+          $table->string('geschlecht', 1)->nullable();
           $table->date('geburtsdatum')->nullable();
           $table->date('vereinseintritt')->nullable();
           $table->date('vereinsaustritt')->nullable();
-          $table->integer('sportSections_id')->default(0);
+          $table->foreignId('sportSections_id')->default(0);
           $table->string('password_alt', 20)->nullable();
           $table->integer('webspace')->default(0);
           $table->integer('regattatrainer')->default(0);
@@ -32,22 +32,18 @@ class CreateUsersTable extends Migration
           $table->integer('seite')->default(0);
           $table->integer('status')->default(0);
           $table->integer('admin')->default(0);
-          $table->integer('vorstand_id')->default(0);
-          $table->text('beschreibung');
+          $table->foreignId('vorstand_id')->default(0);
+          $table->text('beschreibung')->nullable();
           $table->string('telefon', 25)->default('');
           $table->string('bild', 15)->default('');
           $table->integer('pixx')->default(0);
           $table->integer('pixy')->default(0);
 
-          //Laravel
+          //Laravel 8
           $table->string('name')->nullable();
-
-          $table->string('email')->nullable();
-          //$table->string('email')->unique();
-
+          $table->string('email')->unique();
           $table->timestamp('email_verified_at')->nullable();
           $table->string('password');
-
           $table->rememberToken();
           $table->foreignId('current_team_id')->nullable();
           $table->text('profile_photo_path')->nullable();
@@ -68,79 +64,6 @@ class CreateUsersTable extends Migration
     }
 }
 
-
-
-/*
-$table->id();
-//Vereinsverwaltung
-$table->string('nachname', 40)->default('');
-$table->string('vorname', 40)->default('');
-$table->string('geschlecht', 1);
-$table->date('geburtsdatum')->nullable();
-$table->date('vereinseintritt')->nullable();
-$table->date('vereinsaustritt')->nullable();
-$table->integer('idgruppe')->default(0);
-$table->string('password_alt', 20)->default('');
-//laravel
-$table->string('password');
-//Vereinsverwaltung
-$table->integer('admin')->default(0);
-$table->integer('idvorstand')->default(0);
-$table->text('beschreibung');
-//Laravel
-$table->string('email')->unique();
-//$table->string('email')->unique();
-//Vereinsverwaltung
-$table->string('telefon', 25)->default('');
-$table->string('bild', 15)->default('');
-$table->integer('pixx')->default(0);
-$table->integer('pixy')->default(0);
-//Laravel
-$table->string('name');
-//Vereinsverwaltung
-$table->integer('webspace')->default(0);
-$table->integer('regattatrainer')->default(0);
-$table->char('trainernachricht', 1)->default('');
-$table->decimal('gewicht', 4, 1)->default(0.0);
-$table->integer('position')->default(0);
-$table->integer('seite')->default(0);
-$table->integer('status')->default(0);
-//Laravel
-$table->timestamp('email_verified_at')->nullable();
-$table->rememberToken();
-$table->foreignId('current_team_id')->nullable();
-$table->text('profile_photo_path')->nullable();
-$table->timestamps();
-
-/*
-Vereinsverwaltung
-$table->integer('idmitglied', true);
-$table->string('nachname', 40)->default('');
-$table->string('vorname', 40)->default('');
-$table->string('geschlecht', 1);
-$table->date('geburtsdatum');
-$table->date('vereinseintritt');
-$table->date('vereinsaustritt');
-$table->integer('idgruppe')->default(0);
-$table->string('password', 20)->default('');
-$table->string('passwort');
-$table->integer('admin')->default(0);
-$table->integer('idvorstand')->default(0);
-$table->binary('beschreibung', 65535);
-$table->string('email', 60);
-$table->string('telefon', 25)->default('');
-$table->string('bild', 15)->default('');
-$table->integer('pixx')->default(0);
-$table->integer('pixy')->default(0);
-$table->string('loginname', 40)->default('');
-$table->integer('webspace')->default(0);
-$table->integer('regattatrainer')->default(0);
-$table->char('trainernachricht', 1)->default('');
-$table->decimal('gewicht', 4, 1)->default(0.0);
-$table->integer('position')->default(0);
-$table->integer('seite')->default(0);
-$table->integer('status')->default(0);
-*/
 
 /* Orginal
 Schema::create('users', function (Blueprint $table) {

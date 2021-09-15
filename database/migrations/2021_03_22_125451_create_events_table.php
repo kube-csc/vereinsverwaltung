6c172d9113d181b729c6baa03e71be11cb53728b
kube-csc <kube@kube-csc.de>
2021-09-15 21:51:07 +0200

    Seeder Anpassung der Feldernamen und Tabelle von events

--- database/migrations/2021_03_22_125451_create_events_table.php
+++ database/migrations/2021_03_22_125451_create_events_table.php
@@ -14,56 +14,54 @@ class CreateEventsTable extends Migration
     public function up()
     {
         Schema::create('events', function (Blueprint $table) {
-          $table->id();
-          $table->date('datumvon');
-    			$table->date('datumbis');
-    			$table->date('datumvona')->nullable();
-    			$table->date('datumbisa')->nullable();
-    			$table->string('veranstaltung', 50)->default('');   // TODO: wird nicht mehr benötigt
-    			$table->string('ueberschrift', 50)->default('');
-    			$table->text('beschreibung')->nullable();
-    			$table->string('ansprechparter', 50)->nullable();
-    			$table->string('telefon', 25)->nullable();
-    			$table->string('email', 50)->nullable();
-    			$table->string('homepage')->nullable();
-    			//$table->unsignedBigInteger('gruppe');
-          $table->unsignedBigInteger('sportSection_id')->nullable();    //TODO: Das Feld soll die Gruppe ablösen
-    			$table->char('startseite', 1)->default('');
-    			$table->text('nachtermin')->nullable();
-    			$table->text('anmeldetext')->nullable();
-    			$table->char('onlinemeldung', 3)->default('');
-    			//$table->date('erstelldatum');  //TODO: Soll durch update_at
-    			$table->char('regatta')->default('');
-    			$table->integer('verwendung')->default(0);
-    			                                              	    // 0 = Event / Termin
-                                                              // 1 =
-                                                              // 2 =
-                                                              // 3 =
-    			                                                    // 4 = Abteilungsbeschreibung
-    			$table->unsignedBigInteger('eventGroup_id')->nullable();
-    			$table->text('einverstaendnis')->nullable();
-    			$table->integer('teilnehmer')->default(0);
-    			$table->integer('teilnehmermax')->default(0);
-    			$table->unsignedBigInteger('autor_id')->nullable();
-    			$table->unsignedBigInteger('bearbeiter_id')->nullable();
-    			//$table->date('bearbeitungsdatum')->nullable();
-    			$table->char('freigabe', 1)->default('');
-          $table->SoftDeletes();
-          $table->timestamps();
+                $table->id();
+                $table->date('datumvon');
+                $table->date('datumbis');
+                $table->date('datumvona')->nullable();
+                $table->date('datumbisa')->nullable();
+                $table->string('veranstaltung', 50)->default('');   // ToDo: wird nicht mehr benötigt
+                $table->string('ueberschrift', 50)->default('');
+                $table->text('beschreibung')->nullable();
+                $table->string('ansprechparter', 50)->nullable();
+                $table->string('telefon', 25)->nullable();
+                $table->string('email', 50)->nullable();
+                $table->string('homepage')->nullable();
+                //$table->unsignedBigInteger('gruppe'); // jetzt sportSection_id
+                $table->unsignedBigInteger('sportSection_id')->nullable();
+                $table->char('startseite', 1)->default('');
+                $table->text('nachtermin')->nullable();
+                $table->text('anmeldetext')->nullable();
+                $table->char('onlinemeldung', 3)->default('');
+                //$table->date('erstelldatum');  //jetzt update_at
+                $table->char('regatta')->default('');
+                $table->integer('verwendung')->nullable();
+                                                                    // 0 = Event / Termin
+                                                                    // 1 =
+                                                                    // 2 =
+                                                                    // 3 =
+                                                                    // 4 = Abteilungsbeschreibung
+                $table->unsignedBigInteger('eventGroup_id')->nullable();
+                $table->text('einverstaendnis')->nullable();
+                $table->integer('teilnehmer')->default(0);
+                $table->integer('teilnehmermax')->default(0);
+                $table->unsignedBigInteger('autor_id')->nullable();
+                $table->unsignedBigInteger('bearbeiter_id')->nullable();
+                //$table->date('bearbeitungsdatum')->nullable();
+                $table->char('freigabe', 1)->default('');
+                $table->SoftDeletes();
+                $table->timestamps();
 
-    			$table->foreign('autor_id')
-    			  ->references('id')->on('users');
+                $table->foreign('autor_id')
+                  ->references('id')->on('users');
 
-    			$table->foreign('bearbeiter_id')
-    			  ->references('id')->on('users');
+                $table->foreign('bearbeiter_id')
+                  ->references('id')->on('users');
 
-          $table->foreign('sportSection_id')
-            ->references('id')->on('sport_sections');
+                $table->foreign('sportSection_id')
+                ->references('id')->on('sport_sections');
 
-          $table->foreign('eventGroup_id')
-    			  ->references('id')->on('event_groups');
-
-
+                $table->foreign('eventGroup_id')
+                  ->references('id')->on('event_groups');
         });
     }
 
@@ -74,6 +72,12 @@ class CreateEventsTable extends Migration
      */
     public function down()
     {
+        Schema::table('events', function (Blueprint $table) {
+            $table->dropForeign('autor_id');
+            $table->dropForeign('bearbeiter_id');
+            $table->dropForeign('sportSection_id');
+            $table->dropForeign('eventGroup_id');
+        });
         Schema::dropIfExists('events');
     }
 }

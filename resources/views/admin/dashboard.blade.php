64183dae9fa8e03325824a63f0f1e9dd77ca0ea1
kube-csc <kube@kube-csc.de>
2021-09-21 23:09:19 +0200

    Dashoboard Menu mit link zum Internen erstellt

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -8,7 +8,84 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+
+                <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+                    <div class="flex justify-center pt-8 sm:justify-start sm:pt-0 text-xl">
+                     Persönliches Deshboard
+                    </div>
+
+                    <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
+                        <div class="grid grid-cols-1 md:grid-cols-2">
+                            <div class="p-6">
+                                <div class="flex items-center">
+                                     <div class="ml-4 text-lg leading-7 font-semibold">Interne Bereich</div>
+                                </div>
+
+                                <div class="ml-12">
+                                    <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+                                        <div class="rounded border shadow p-3 my-2 bg-blue-200" onclick="window.location.replace('/Adminmenu')">
+                                            <div class="justify-between my-2">
+                                                <div class="flex">
+                                                    <p class="font-bold text-lg">
+                                                        <a  href="{{ url('/Adminmenu') }}">Zum Internen Bereich</a>
+                                                    </p>
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l">
+                                <div class="flex items-center">
+                                      <div class="ml-4 text-lg leading-7 font-semibold"> </div>
+                                </div>
+
+                                <div class="ml-12">
+                                    <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="p-6 border-t border-gray-200 dark:border-gray-700">
+                                <div class="flex items-center">
+                                    <div class="ml-4 text-lg leading-7 font-semibold">  </div>
+                                </div>
+
+                                <div class="ml-12">
+                                    <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-l">
+                                <div class="flex items-center">
+                                    <div class="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-white">  </div>
+                                </div>
+
+                                <div class="ml-12">
+                                    <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
+                        <div class="text-center text-sm text-gray-500 sm:text-left">
+
+                        </div>
+
+                        <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
+
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>

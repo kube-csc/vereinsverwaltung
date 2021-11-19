3c9061976b5d5578a21bba06e3958f6c48a7a500
kube-csc <kube@kube-csc.de>
2021-11-19 01:10:00 +0100

    bug Fix Dokumenenanzeige bei den Frondend Wenn keine Dokumente vorhanden sind werden im Footer keine Dokumente eingeblendet

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -137,20 +137,36 @@ class HomeController extends Controller
 
     public function eventShow($eventSeorch)
     {
+        $documents = Document::where('footerStatus' , 1)
+            ->where('startDatum' , '<=' , Carbon::now()->toDateString())
+            ->where('endDatum'   , '>=' , Carbon::now()->toDateString())
+            ->where('dokumentenFile' ,'!=' , NULL)
+            ->get();
         $eventSeorchName = substr ($eventSeorch , 0, -10);
         $dateFor = substr ($eventSeorch, -10);
         $seoch = str_replace('_' , ' ' , $eventSeorchName);
         $events = event::where('ueberschrift' , $seoch)->where('datumvon' , $dateFor)->get();
 
-        return view('home.eventShow' , compact('events'));
+        return view('home.eventShow')->with([
+            'events'      => $events,
+            'documents'   => $documents
+        ]);
     }
 
     public function instructionShow($instructionSeorch)
     {
+        $documents = Document::where('footerStatus' , 1)
+            ->where('startDatum' , '<=' , Carbon::now()->toDateString())
+            ->where('endDatum'   , '>=' , Carbon::now()->toDateString())
+            ->where('dokumentenFile' ,'!=' , NULL)
+            ->get();
         $seoch = str_replace('_' , ' ' , $instructionSeorch);
         $instructions = instruction::where('ueberschrift' , $seoch)->get();
 
-        return view('instruction.show' , compact('instructions'));
+        return view('instruction.show')->with([
+            'instructions'      => $instructions,
+            'documents'         => $documents
+        ]);
     }
 
     public function eventFutureAll()

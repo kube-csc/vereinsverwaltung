1147251b1124021d08e125363a1f302bedee3fc5
kube-csc <kube@kube-csc.de>
2022-01-27 00:22:54 +0100

    Bug beseitigt Backlinks aus dem Hauptmenu verschwunden

--- resources/views/admin/adminmenu.blade.php
+++ resources/views/admin/adminmenu.blade.php
@@ -180,6 +180,15 @@
                                       </div>
                                   </div>
 
+                                  <div class="rounded border shadow p-3 my-2 bg-blue-200" onclick="window.location.replace('Backlink/alle')">
+                                      <div class="flex justify-between my-2">
+                                          <div class="flex">
+                                              <p class="font-bold text-lg">Backlinks alter Weblayout</p>
+                                              <p class="mx-3 py-1 text-xs text-gray-500 font-semibold"></p>
+                                          </div>
+                                      </div>
+                                  </div>
+
                               </div>
 
                          </div>

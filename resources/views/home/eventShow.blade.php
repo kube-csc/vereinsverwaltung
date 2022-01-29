3c5e8ec5e958da598a325bad4d31d264ec9f5e8f
kube-csc <kube@kube-csc.de>
2022-01-29 19:07:05 +0100

    eventGallery neu angelegt und eingebunden Orginal Code von der section portfolio in der index.php

--- resources/views/home/eventShow.blade.php
+++ resources/views/home/eventShow.blade.php
@@ -1,92 +1,88 @@
 @extends('layouts.frontend')
 
-@section('title' ,'Event')
+@section('title' ,'Termin')
 
 @section('content')
 
-    <main id="main">
+  <main id="main">
 
-    <!-- ======= About Section ======= -->
-        <section id="about" class="about">
-            <div class="container">
-                <div class="row no-gutters">
-            <?php
-            foreach ($events as $event)
-             {
-             ?>
-                    <div class="content col-xl-5 d-flex align-items-stretch" data-aos="fade-up">
-                        <div class="content">
-                            <!-- ======= Facebook======= -->
-                            @if(env('Verein_Sozialmediaanzeigen')=='ja')
-                             <center>
-                               <div class="fb-like" data-href="http://www.{{ str_replace('_' , ' ' , env('Verein_Domain')) }} data-send="true" data-layout="box_count" data-width="183" data-show-faces="true" data-font="arial"></div>
-                             </center>
+  @foreach ($events as $event)
+
+    <section id="portfolio" class="portfolio">
+        <div class="container">
+            <div class="section-title" data-aos="fade-in" data-aos-delay="100">
+                <!-- ======= Facebook======= -->
+                @if(env('Verein_Sozialmediaanzeigen')=='ja')
+                <center>
+                    <div class="fb-like" data-href="http://www.{{ str_replace('_' , ' ' , env('Verein_Domain')) }} data-send="true" data-layout="box_count" data-width="183" data-show-faces="true" data-font="arial"></div>
+                </center>
+                @endif
+
+                <h2>{{ $event->ueberschrift }}</h2>
+
+                @if($event->datumvon == $event->datumbis)
+                    <p>am {{ date("d.m.Y", strtotime($event->datumvon)) }}</p>
+                @else
+                    <p>von {{ date("d.m.Y", strtotime($event->datumvon)) }} bis {{ date("d.m.Y", strtotime($event->datumbis)) }}</p>
+                @endif
+                @if($event->datumbisa <= Illuminate\Support\Carbon::now() && isset($event->datumbisa))
+                    <p>
+                        <b>Anmeldezeitraum:</b><br>
+                        @if(isset($event->datumvona))
+                            von {{ date("d.m.Y", strtotime($event->datumvona)) }}<br>
+                        @endif
+                            bis {{ date("d.m.Y", strtotime($event->datumbisa)) }}
+                    </p>
+                @endif
+
+                @if(isset($event->homepage))
+                    <p>
+                        <b>Homepage:</b><br>
+                        {{ $event->homepage }}
+                    </p>
+                @endif
+
+                @if($event->datumbis > Illuminate\Support\Carbon::now())
+                    @if(isset($event->ansprechparter) or isset($event->telefon) or isset($event->email))
+                        <p>
+                            <b>Ansprechpartner:</b><br>
                             @endif
-                            <h3>{{ $event->ueberschrift }}</h3>
-                        @if($event->datumvon == $event->datumbis)
-                            <p>am {{ date("d.m.Y", strtotime($event->datumvon)) }}</p>
-                        @else
-                            <p>von {{ date("d.m.Y", strtotime($event->datumvon)) }} bis {{ date("d.m.Y", strtotime($event->datumbis)) }}</p>
-                        @endif
-                        @if($event->datumbisa <= Illuminate\Support\Carbon::now() && isset($event->datumbisa))
-                            <p>
-                                <b>Anmeldezeitraum:</b><br>
-                                @if(isset($event->datumvona))
-                                 von {{ date("d.m.Y", strtotime($event->datumvona)) }}<br>
-                                @endif
-                                 bis {{ date("d.m.Y", strtotime($event->datumbisa)) }}
-                            </p>
-                        @endif
-                        @if(isset($event->homepage))
-                            <p>
-                                <b>Homepage:</b><br>
-                                {{ $event->homepage }}
-                            </p>
-                        @endif
-                        @if($event->datumbis > Illuminate\Support\Carbon::now())
+                            @if(isset($event->ansprechparter))
+                                {{ $event->ansprechparter }}<br>
+                            @endif
+                            @if(isset($event->telefon))
+                                <b>Telefon:</b><br>
+                                {{ $event->telefon }}<br>
+                            @endif
+                            @if(isset($event->email))
+                                <b>E-Mail:</b><br>
+                                {{ $event->email }}
+                            @endif
                             @if(isset($event->ansprechparter) or isset($event->telefon) or isset($event->email))
-                                <p>
-                                    <b>Ansprechpartner:</b><br>
-                                    @endif
-                                    @if(isset($event->ansprechparter))
-                                        {{ $event->ansprechparter }}<br>
-                                    @endif
-                                    @if(isset($event->telefon))
-                                        <b>Telefon:</b><br>
-                                        {{ $event->telefon }}<br>
-                                    @endif
-                                    @if(isset($event->email))
-                                        <b>E-Mail:</b><br>
-                                        {{ $event->email }}
-                                    @endif
-                                    @if(isset($event->ansprechparter) or isset($event->telefon) or isset($event->email))
-                                </p>
-                            @endif
-                                <p>
-                                  {!! $event->beschreibung !!}
-                                </p>
-                        @endif
-                        @if($event->datumvon < Illuminate\Support\Carbon::now())
-                            @if(isset($event->homepage) && $event->homepage <> '')
-                               <p>Homepage: {!! $event->homepage !!}</p>
-                            @endif
-                                    <p>{!! $event->nachtermin !!}</p>
-                        @endif
-                       </div>
-                    </div>
-             @php
-             }
-             @endphp
-                    <div class="col-xl-7 d-flex align-items-stretch">
-                        <div class="icon-boxes d-flex flex-column justify-content-center">
+                        </p>
+                    @endif
+                    <br>
+                    <p>
+                        {!! $event->beschreibung !!}
+                    </p>
+                @endif
 
+                @if($event->datumvon < Illuminate\Support\Carbon::now())
+                    @if(isset($event->homepage) && $event->homepage <> '')
+                       <br>
+                       <p>Homepage: {!! $event->homepage !!}</p>
+                    @endif
+                    <p>{!! $event->nachtermin !!}</p>
+                @endif
 
-                        </div><!-- End .content-->
-                    </div>
-                </div>
             </div>
-        </section><!-- End About Section -->
 
-    </main><!-- End #main -->
+            <livewire:event-gallery />
+
+        </div>
+    </section><!-- End Portfolio Section -->
+  @endforeach
+
+  </main><!-- End #main -->
 
 @endsection

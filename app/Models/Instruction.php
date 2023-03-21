779bcbb2a547db2a21fcda7cad00ed307dd1bbae
kube-csc <kube@kube-csc.de>
2023-03-22 00:07:45 +0100

    Name Instruction.php angepasst

--- app/Models/Instruction.php
+++ app/Models/Instruction.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class instruction extends Model
+class Instruction extends Model
 {
     use HasFactory;
     use SoftDeletes;

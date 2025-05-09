627eab8f726cd7c6d5289547fb82a5c2c3141579
kube-csc <kube@kube-csc.de>
2025-05-09 22:16:32 +0200

    Füge Kommentar zur Datei für die Passwortzurücksetzung hinzu Ergänze Tabelle für Passwortrücksetzung

---
+++ database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+// This file is part of the Laravel framework von de App kurse
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('password_reset_tokens', function (Blueprint $table) {
+            $table->string('email')->primary();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('password_reset_tokens');
+    }
+};
--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+// This file is part of the Laravel framework von de App vereinsverwaltung_dev
+
 class CreatePasswordResetsTable extends Migration
 {
     /**
